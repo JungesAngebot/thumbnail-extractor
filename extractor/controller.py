765dcf770de425a06a48cfcd0b7f81ea5daf281a
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:06:36 +0200

    - made controller method static

--- extractor/controller.py
+++ extractor/controller.py
@@ -6,6 +6,7 @@
 class AnalysisController(object):
     """ Basically starts the analysis process for a specified video"""
 
-    def analyze_video(self):
+    @staticmethod
+    def analyze_video():
         pass
 
