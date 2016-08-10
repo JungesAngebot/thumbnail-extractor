b687fddd9d1de4164d5688e47ef4a41204b5cf1e
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:42:53 +0200

    - doc

--- extractor/flask_app.py
+++ extractor/flask_app.py
@@ -12,6 +12,7 @@ This module creates the flask instance and configures the system environment.
 
 
 def create_app():
+    """ Creates and configures the flask instance. """
     app = Flask(__name__, static_folder=APP_ROOT + '/static', static_url_path='/static',
                 template_folder=APP_ROOT + '/templates')
     app.register_blueprint(frontend)
@@ -19,4 +20,8 @@ def create_app():
 
 
 def configure_environment():
+    """ Sets the required env variables.
+
+    Eg. the google auth env variable will be set to point to the auth file used for the vision api.
+    """
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '%s/config/junges_angebot.json' % APP_ROOT
