4c16d8465191d7b8c678d81193a6e49eeff77fba
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:34:09 +0200

    - dominant color calc

--- extractor/image_analysis.py
+++ extractor/image_analysis.py
@@ -1,5 +1,13 @@
+from colorthief import ColorThief
 
 
 def determine_dominant_color_for_images(images):
-    pass
+    for image in images:
+        color_thief = ColorThief(image.image_name)
+        dominant_color = 'nocolor'
+        try:
+            dominant_color = color_thief.get_color(quality=1)
+        except:
+            pass
+        image.dominant_color = dominant_color
 
