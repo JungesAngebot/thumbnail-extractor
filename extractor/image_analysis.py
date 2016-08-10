44cb8fc17ca42bc3ec465f151c94fa0e9ff7e104
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:35:43 +0200

    - doc

--- extractor/image_analysis.py
+++ extractor/image_analysis.py
@@ -10,6 +10,13 @@ Analysis:
 
 
 def determine_dominant_color_for_images(images):
+    """ Detects the dominant color for each given image.
+
+    To detect the dominant color this function uses color thief.
+
+    :param images: images to detect the dominant color
+    :return: nothing (call by reference)
+    """
     for image in images:
         color_thief = ColorThief(image.image_name)
         dominant_color = 'nocolor'
