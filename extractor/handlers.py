79daa83bc009a3aa9ffd5014d277d16db20def80
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:38:07 +0200

    - doc

--- extractor/handlers.py
+++ extractor/handlers.py
@@ -7,6 +7,7 @@ from extractor import frontend, APP_ROOT
 
 
 def clean_env():
+    """ Cleans the static/images folder. """
     files = glob.glob('%s/static/images/*' % APP_ROOT)
     for f in files:
         os.remove(f)
