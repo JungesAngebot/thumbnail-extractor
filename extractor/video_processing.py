4d3492d9988594ec4307e26f1f0eaeb568585829
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:14:56 +0200

    - ffmpeg command building

--- extractor/video_processing.py
+++ extractor/video_processing.py
@@ -2,6 +2,8 @@ import os
 
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
+from extractor import APP_ROOT
+
 
 def get_duration_of_video(video_filename):
     """ Returns the duration for the given video file. """
@@ -9,6 +11,17 @@ def get_duration_of_video(video_filename):
     return int(clip.duration)
 
 
+def build_ffmpeg_command_to_extract_frame(second, video_filename, counter):
+    command_template = 'ffmpeg -i %s -ss 00:00:%s.32 -vframes 1 %s/static/images/out%s.png'
+
+    return command_template % (video_filename, second, APP_ROOT, counter)
+
+
 def extract_frames_from_video(video_filename):
     if not os.path.isfile(video_filename):
         raise FileNotFoundError('Cannot find file %s.' % video_filename)
+    image_names = []
+    second = 0
+    for i in range(get_duration_of_video(video_filename)):
+        pass
+    return image_names
