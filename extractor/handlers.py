db0e12f7d14f1f552f75505709d60199acd212e4
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 16:57:53 +0200

    - new starting

--- extractor/handlers.py
+++ extractor/handlers.py
@@ -1,11 +1,8 @@
 from flask import render_template, request
 
 from extractor import frontend
-from extractor.controller import extract_frames
 
 
 @frontend.route('/')
 def show_index_page():
-    generate_thumbnails = request.args['gen'] if 'gen' in request.args else False
-    images = extract_frames()
-    return render_template('index.html', images=images)
+    return render_template('index.html')
