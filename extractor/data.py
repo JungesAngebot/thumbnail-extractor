6f0042e87d998431e8cf03ba5f3422285cf41df3
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:28:48 +0200

    - module doc

--- extractor/data.py
+++ extractor/data.py
@@ -1,9 +1,13 @@
 
+
+""" Simple module for representing data to be rendered. """
+
+
 class Image(object):
 
     def __init__(self, image_name):
         self.image_name = image_name
-        
+
     @classmethod
     def create_with_image_name(cls, image_name):
         return cls(image_name)
