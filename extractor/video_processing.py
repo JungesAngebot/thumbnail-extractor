c86b158c47f8673b87e8f12ca9f647fa85cb4a65
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:12:05 +0200

    - implemented get duration for video

--- extractor/video_processing.py
+++ extractor/video_processing.py
@@ -1,5 +1,12 @@
 import os
 
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+
+def get_duration_of_video(video_filename):
+    clip = VideoFileClip(video_filename)
+    return int(clip.duration)
+
 
 def extract_frames_from_video(video_filename):
     if not os.path.isfile(video_filename):
