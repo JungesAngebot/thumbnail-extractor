6e1a0eba7f66067e42bd8275160bef1776fbf7cb
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:06:16 +0200

    - commenting

--- extractor/controller.py
+++ extractor/controller.py
@@ -1,5 +1,8 @@
 
 
+""" Basic controller layer for hooking up the analyzis functionality. """
+
+
 class AnalysisController(object):
     """ Basically starts the analysis process for a specified video"""
 
