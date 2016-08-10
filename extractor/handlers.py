005b213450c3379cf7fc22bbbad9f2fad954cc6c
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:37:33 +0200

    - cleaning env before next processing

--- extractor/handlers.py
+++ extractor/handlers.py
@@ -1,8 +1,18 @@
+import glob
+import os
+
 from flask import render_template, request
 
-from extractor import frontend
+from extractor import frontend, APP_ROOT
+
+
+def clean_env():
+    files = glob.glob('%s/static/images/*' % APP_ROOT)
+    for f in files:
+        os.remove(f)
 
 
 @frontend.route('/')
 def show_index_page():
+    clean_env()
     return render_template('index.html')
