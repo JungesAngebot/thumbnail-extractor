6b3f6bec86805c7269d31483251fa33eade18ae1
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:22:10 +0200

    - doc

--- extractor/video_processing.py
+++ extractor/video_processing.py
@@ -30,6 +30,13 @@ def build_ffmpeg_command_to_extract_frame(second, video_filename, counter):
 
 
 def extract_frames_from_video(video_filename):
+    """ Extracts frames from the fiven video.
+
+    Only takes the first minute of the video to extract frames from it. Frame
+    extraction is done video ffmpeg and the resulting images are stored as png under
+    static/images. All resulting images following the naming pattern
+    out<NumberOfTheImage>.png. This makes it easier to get the images for further processing.
+    """
     if not os.path.isfile(video_filename):
         raise FileNotFoundError('Cannot find file %s.' % video_filename)
     image_names = []
