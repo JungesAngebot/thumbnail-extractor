c851521bf6299b62c9e6289caba7d3a7a9ff0c8d
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-10 20:23:10 +0200

    - basic landmark parsing

--- extractor/data.py
+++ extractor/data.py
@@ -16,7 +16,27 @@ class Image(object):
     def image_stream(self):
         return open(self.image_name, 'rb')
 
+    def set_face_detection_result(self, result):
+        for landmark in result[0]['landmarks']:
+            pass
+
     @classmethod
     def create_with_image_name(cls, image_name, static_url_ref):
         """ Creates an instance of the image class based on a file name"""
         return cls(image_name, static_url_ref)
+
+
+class Landmark(object):
+    def __init__(self, landmark_type, x, y, z):
+        self.landmark_type = landmark_type
+        self.x = x
+        self.y = y
+        self.z = z
+
+    @classmethod
+    def create_from_dict(cls, landmark_dict):
+        return cls(landmark_dict['type'],
+                   landmark_dict['position']['x'],
+                   landmark_dict['position']['y'],
+                   landmark_dict['position']['y'])
+
