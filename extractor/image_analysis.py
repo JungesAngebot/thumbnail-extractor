f685a6ca4770466dc49f4ca4cf404a732d39881c
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-10 17:44:57 +0200

    - added google vision call for auth

--- extractor/image_analysis.py
+++ extractor/image_analysis.py
@@ -1,5 +1,6 @@
 from colorthief import ColorThief
-
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
 
 """ This module performs analysis of given images.
 
@@ -7,6 +8,9 @@ Analysis:
 - detect dominant color
 - detect faces in an image
 """
+
+
+DISCOVERY_URL = 'https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
 
 
 def determine_dominant_color_for_images(images):
@@ -26,3 +30,8 @@ def determine_dominant_color_for_images(images):
             pass
         image.dominant_color = dominant_color
 
+
+def get_vision_service():
+    credentials = GoogleCredentials.get_application_default()
+    return discovery.build('vision', 'v1', credentials=credentials,
+                           discoveryServiceUrl=DISCOVERY_URL)
