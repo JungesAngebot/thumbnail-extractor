ac1c8bff284c782e68a0e039fad9f6e14e8b11ab
paddyws <patrick.wilmes@bit-lake.com>
2016-08-10 17:34:48 +0200

    - module doc

--- extractor/image_analysis.py
+++ extractor/image_analysis.py
@@ -1,6 +1,14 @@
 from colorthief import ColorThief
 
 
+""" This module performs analysis of given images.
+
+Analysis:
+- detect dominant color
+- detect faces in an image
+"""
+
+
 def determine_dominant_color_for_images(images):
     for image in images:
         color_thief = ColorThief(image.image_name)
