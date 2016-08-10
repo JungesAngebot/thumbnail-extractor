5e505f2fa753b62753ed09c4c8c86edc3713aeeb
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-10 17:45:38 +0200

    - added google vision call for face detection

--- extractor/image_analysis.py
+++ extractor/image_analysis.py
@@ -1,3 +1,5 @@
+import base64
+
 from colorthief import ColorThief
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
@@ -35,3 +37,32 @@ def get_vision_service():
     credentials = GoogleCredentials.get_application_default()
     return discovery.build('vision', 'v1', credentials=credentials,
                            discoveryServiceUrl=DISCOVERY_URL)
+
+
+def detect_face(face_file, max_results=4):
+    """Uses the Vision API to detect faces in the given file.
+    Args:
+        face_file: A file-like object containing an image with faces.
+    Returns:
+        An array of dicts with information about the faces in the picture.
+        :param face_file:
+        :param max_results:
+    """
+    image_content = face_file.read()
+    batch_request = [{
+        'image': {
+            'content': base64.b64encode(image_content).decode('UTF-8')
+        },
+        'features': [{
+            'type': 'FACE_DETECTION',
+            'maxResults': max_results,
+        }]
+    }]
+
+    service = get_vision_service()
+    request = service.images().annotate(body={
+        'requests': batch_request,
+    })
+    response = request.execute()
+
+    return response['responses'][0]['faceAnnotations'] if 'faceAnnotations' in response['responses'][0] else None
