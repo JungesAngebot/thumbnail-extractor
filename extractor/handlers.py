6093b68edaeefbe3c26230330613598bf968f802
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-11 10:18:56 +0200

    - added hook for detail view

--- extractor/handlers.py
+++ extractor/handlers.py
@@ -28,3 +28,12 @@ def show_thumbnails():
     """ Hook for displaying all thumbnails generated. """
     images = AnalysisController.get_all_generated_thumbnails()
     return render_template('overview.html', images=images, page='overview')
+
+
+@frontend.route('/thumbnails/<string:image_name>')
+def show_single_thumbnail_by_name(image_name):
+    """ Hook for displaying a single thumbnail.
+
+    The thumbnail and all detail information will be shown on a single page.
+    """
+    return render_template('detail.html')
