a5c78a57302a74e529139339a2971ebada31ccf2
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-11 14:29:22 +0200

    - audio extraction

--- extractor/audio_analysis.py
+++ extractor/audio_analysis.py
@@ -1,2 +1,8 @@
+import os
+
+
 def extract_audio_from_video(video_name):
-    pass
+    """ Extracts audio from a video clip. """
+    command = 'ffmpeg -i %s %s/static/audios/out.mp3' % video_name
+    os.system(command)
+    return 'out.mp3'
