d4a493e0d2d32f96cdcd94c8ebfd30ee7799a8da
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-11 14:30:55 +0200

    - speed and beats determination

--- extractor/audio_analysis.py
+++ extractor/audio_analysis.py
@@ -1,8 +1,18 @@
 import os
+import librosa
+
+from extractor import APP_ROOT
 
 
 def extract_audio_from_video(video_name):
     """ Extracts audio from a video clip. """
     command = 'ffmpeg -i %s %s/static/audios/out.mp3' % video_name
     os.system(command)
-    return 'out.mp3'
+
+
+def get_speed_and_beats_from_audio():
+    """ Determines the speed and beats of an audio file. """
+    y, sr = librosa.load('%s/static/audios/out.mp3' % APP_ROOT)
+    tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+    return tempo, beats
+
