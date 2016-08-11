82378ebd4924c749b03657f7e3a20ee0c194d1b3
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-11 15:55:20 +0200

    - reformat

--- extractor/thumbnail_rating.py
+++ extractor/thumbnail_rating.py
@@ -1,8 +1,9 @@
-
 def determine_best(images):
     """ function determines the best thumbnail out of the given list.
 
     It checks if the image has a successful face detection, joy or surprise likelihood during face detection
     and checks that the dominant color is not to dark or to bright.
     """
-    pass
+    candidate = None
+
+    return candidate
