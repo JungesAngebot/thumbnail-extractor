af631ef728f5d11df853ee2db8d53b2c144893c1
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-11 11:31:04 +0200

    - function to highlight faces in an image

---
+++ extractor/rendering.py
@@ -0,0 +1,23 @@
+from PIL import Image
+from PIL import ImageDraw
+
+
+def highlight_faces(image, faces, output_filename):
+    """Draws a polygon around the faces, then saves to output_filename.
+
+    Args:
+      image: a file containing the image with the faces.
+      faces: a list of faces found in the file. This should be in the format
+          returned by the Vision API.
+      output_filename: the name of the image file to be created, where the faces
+          have polygons drawn around them.
+    """
+    im = Image.open(image)
+    draw = ImageDraw.Draw(im)
+
+    for face in faces:
+        box = [(v.get('x', 0.0), v.get('y', 0.0)) for v in face['fdBoundingPoly']['vertices']]
+        draw.line(box + [box[0]], width=5, fill='#00ff00')
+
+    del draw
+    im.save(output_filename)
