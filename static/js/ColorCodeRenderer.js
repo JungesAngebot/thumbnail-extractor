a00a226f2b3f71e5578c96f1f072d9bdfc20e655
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-11 11:52:07 +0200

    - better rendering

---
+++ static/js/ColorCodeRenderer.js
@@ -0,0 +1,15 @@
+/**
+ * Created by patrick on 11/08/16.
+ */
+
+var ColorCodeRenderer = function() {
+
+    function init() {
+        $('#color-codes')
+    }
+
+    return {
+        init: init
+    }
+
+};
