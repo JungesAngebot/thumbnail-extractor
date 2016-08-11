88b815dd829cb81570985735fff85e6761e3b469
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-11 09:22:46 +0200

    - owl configuration

--- static/js/loaderjs.js
+++ static/js/loaderjs.js
@@ -2,8 +2,12 @@
  * Created by patrick on 09.08.16.
  */
 
-$(document).ready(function(){
-   $('.owl-carousel').owlCarousel({
-            nav: true
-        })
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        autoPlay: 3000,
+
+        items: 4,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3]
+    })
 });
