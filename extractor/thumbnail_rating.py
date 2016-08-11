6219fa8116ab89b9b1152095b85e707818dfd9d4
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-11 16:07:56 +0200

    - calculation impl

--- extractor/thumbnail_rating.py
+++ extractor/thumbnail_rating.py
@@ -1,13 +1,12 @@
 def determine_best(images):
     """ function determines the best thumbnail out of the given list.
 
-    It checks if the image has a successful face detection, joy or surprise likelihood during face detection
-    and checks that the dominant color is not to dark or to bright.
+    It checks if the image has a successful face detection, joy or surprise likelihood during face detection.
     """
     candidate = None
     for image in images:
-        is_friendly_image = has_image_joy_likelihook(image) or has_image_surprise_likelihood(image)
-        is_candidate = has_image_faces(image) and is_friendly_image and is_dominant_color_normal(image)
+        is_friendly_image = has_image_joy_likelihood(image) or has_image_surprise_likelihood(image)
+        is_candidate = has_image_faces(image) and is_friendly_image
         candidate = image if is_candidate else None
     return candidate
 
@@ -16,13 +15,9 @@ def has_image_faces(image):
     return image.landmarks is not None
 
 
-def has_image_joy_likelihook(image):
-    pass
+def has_image_joy_likelihood(image):
+    return image.joy_likelihood == 'VERY_LIKELY'
 
 
 def has_image_surprise_likelihood(image):
-    pass
-
-
-def is_dominant_color_normal(image):
-    pass
+    return image.surprise_likelihood == 'VERY_LIKELY'
