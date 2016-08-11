f403565301db60953cd46c95c3973fae46047d98
pythonixpp <patrick.wilmes@bit-lake.com>
2016-08-11 10:06:17 +0200

    - function to get all images

--- extractor/system_utils.py
+++ extractor/system_utils.py
@@ -11,3 +11,12 @@ def clean_env():
     files = glob.glob('%s/static/images/*' % APP_ROOT)
     for f in files:
         os.remove(f)
+
+
+def get_all_thumbnails():
+    """ Function to get all images prev. generated from a video. """
+    files = glob.glob('%s/static/images/*' % APP_ROOT)
+    images = []
+    for file in files:
+        images.append(file.replace(APP_ROOT, ''))
+    return images
